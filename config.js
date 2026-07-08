d7fb63ff61f4f99e2d720c5a04a506cfc0fef6b0
liahalili464-source <liahalili464@gmail.com>
2026-07-08 19:51:28 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-// Supabase settings
+// MindFlow config
 window.SUPABASE_URL = "https://bdhrwdqjgbqvwbrbseqj.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_j9RFHOCoCjx268vZj8rKIQ_nWXZNAth";
+
+window.GOOGLE_CLIENT_ID = "318354462246-lf80408snlio1aid72tedis2jdgm18ek.apps.googleusercontent.com";
